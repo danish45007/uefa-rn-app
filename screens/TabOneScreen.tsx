e96c4abde3a899d85531af4fd598fec2b09114fc
danish sharma <danish45007@gmail.com>
2021-06-27 22:28:05 +0530

    init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,17 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { ImageBackground, SafeAreaView, StatusBar } from "react-native";
+import globalStyles from "../styles/globalStyles";
+const field = require("../assets/images/field.jpg");
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={globalStyles.droidSafeArea}>
+      <StatusBar hidden={false} barStyle={"dark-content"} />
+      <ImageBackground
+        resizeMode="contain"
+        source={field}
+        style={{ width: "100%", aspectRatio: 2 / 3 }}
+      ></ImageBackground>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ styles/globalStyles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet, Platform } from "react-native";
+export default StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+    alignItems: "center",
+  },
+});
